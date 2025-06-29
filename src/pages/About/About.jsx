6aa1d148af2a18fe5a9f2e57d94f79e835fa44c5
karen-609 <karen_montoya@unitec.edu>
2025-06-28 21:09:49 -0600

    Modificaciones finales de la página About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -19,25 +19,36 @@ const About = () => {
         </h3>
         <div className={styles.content}>
           <p>
-            Creemos que el conocimiento tiene el poder de transformar vidas.
-            Este proyecto nace con la misión de hacer que aprender React sea una
-            experiencia accesible, clara y, sobre todo, motivadora.
+            En CodeFlux creemos que el aprendizaje es un motor que impulsa el
+            crecimiento personal y profesional. Este proyecto nace con la
+            intención de hacer que entender React sea un proceso claro, cercano
+            y alcanzable para cualquier persona con ganas de construir, crear y
+            mejorar.
           </p>
           <br />
           <p>
             Sabemos que empezar en el mundo del desarrollo web puede parecer
             abrumador, pero estamos convencidos de que, con los recursos
             adecuados, cualquiera puede dominar herramientas como React y dar
-            pasos firmes hacia su crecimiento profesional y personal.
+            pasos firmes hacia su crecimiento profesional y personal. No
+            necesitas saberlo todo para empezar, solo necesitas dar el primer
+            paso. Y cada avance, cada error y cada línea de código escrita es
+            una victoria que te acerca a tus metas.
+          </p>
+          <br />
+          <p>
+            CodeFlux es un espacio pensado para quienes quieren aprender de
+            forma práctica, enfrentando retos reales, con recursos que
+            convierten lo complejo en simple y lo teórico en aplicable.
           </p>
           <br />
           <p>
             Aquí encontrarás tips y buenas prácticas que te ayudarán a escribir
             un mejor código al utilizar React, una guía práctica paso a paso
             sobre cómo crear un proyecto en React desde cero e información de
-            interés que no solo te ayudarán a entender cómo funciona React,
-            sino también a desarrollar la confianza para crear tus propios
-            proyectos y enfrentar nuevos desafíos.
+            interés que no solo te ayudarán a entender cómo funciona React, sino
+            también a desarrollar la confianza para crear tus propios proyectos
+            y enfrentar nuevos desafíos.
           </p>
           <br />
           <p>
